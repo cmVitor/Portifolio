717513a856902e7e0eceb268204ebb37c4c2ab48
Vitor <castanheira@discente.ufg.br>
2025-08-04 17:28:05 -0300

    Update skills and Home component with new icons and role descriptions

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
             <div className="home__content">
                 <h2 className='home__hello'>Eu sou,</h2>
                 <h1 className='home__name'> Vitor M. Castanheira</h1>
-                <p className="home__role">Sou: <p><Typewriter texts={['React Developer', 'Dev Full-Stack', 'UI/UX', 'Web Developer']}/></p> </p>
+                <p className="home__role">Sou: <p><Typewriter texts={['React Developer', 'Estágiario em TI', 'UI/UX', 'Web Developer', 'Software Engineering']}/></p> </p>
                 <Link to="/sobre" className="home__button"><span>Sobre mim </span><i class="fa fa-external-link" aria-hidden="true"></i></Link>
                 <div className="home__icons">
                     <a href="https://www.linkedin.com/in/vitor-martins-castanheira-749b1024a/"><i className="fa-brands fa-linkedin"></i></a>
